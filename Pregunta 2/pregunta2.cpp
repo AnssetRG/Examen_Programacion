6d3a13e1c4c789120d7007bf274d4d6a9d3c59fb
JManAn <32886493+JManAn@users.noreply.github.com>
2019-09-14 13:39:27 -0500

    Pregunta 2 arrelgada

--- Pregunta 2/pregunta2.cpp
+++ Pregunta 2/pregunta2.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <string>
-#include <iostream>
 
 using namespace std;
 
 struct Rec{
-    int x;
-    int y;
-    int height;
-    int width;
+    int x,y;
+    int height, width;
+    int Ox, Oy, Fx, Fy;
     Rec(int x, int y, int width, int height){
         this-> x = x;
         this-> y = y;
         this-> width = width;
         this-> height = height;
+        Ox = x - width/2;
+        Fx = x + width/2;
+        Oy = y - height/2;
+        Fy = y + width/2;
     }
 };
 
-void print(int val){
-    cout<<val<<endl;
-}
-
 void Collision(Rec *rec1, Rec *rec2){
-    print(rec1->x);
-    print(rec2->x);
-    print(rec2->width);
-    print(rec2->x + rec2->width);
-    if(rec1->x < rec2->x + rec2->width &&
-    rec2->x < rec1->x + rec1->width &&
-    rec1->y < rec2->y + rec2->height &&
-    rec2->y < rec1->y + rec1->height){
+    if(rec1->Ox < rec2->Fx &&
+    rec2->Ox < rec1->Fx &&
+    rec1->Oy < rec2->Fy &&
+    rec2->Oy < rec1->Fy){
         cout<<"Collision detected"<<endl;
     }else{
         cout<<"None collision detected"<<endl;
@@ -40,7 +34,7 @@ int main()
 {
     Rec *rectangleA = new Rec(2,1,4,2);
     Rec *rectangleB = new Rec(8,3,4,2);
-    Rec *rectangleC = new Rec(4,4,2,4);
+    Rec *rectangleC = new Rec(4,3,2,4);
     Collision(rectangleA, rectangleB);
     Collision(rectangleA, rectangleC);
     Collision(rectangleC, rectangleB);
